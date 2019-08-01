dd6bd3d31de7cdb7577f62f17a755a6de4841c32
Bill <1061241239@nkust.edu>
2019-08-01 15:01:00 +0800

    line

--- app.py
+++ app.py
@@ -26,10 +26,6 @@ def callback():
         abort(400)
     return 'OK'
 
-@app.route("/web")
-def showweb():
-    return '<h1>Hello everyone</h1>'
-
 #處理訊息
 #當訊息種類為TextMessage時，從event中取出訊息內容，藉由TextSendMessage()包裝成符合格式的物件，並貼上message的標籤方便之後取用。
 #接著透過LineBotApi物件中reply_message()方法，回傳相同的訊息內容
