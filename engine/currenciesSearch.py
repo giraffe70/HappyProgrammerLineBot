b225eb7beaf808db2db92191584ac88615148ad7
Bill <1061241239@nkust.edu>
2019-08-01 16:16:11 +0800

    line

--- engine/currenciesSearch.py
+++ engine/currenciesSearch.py
@@ -1,6 +1,6 @@
 import twder
 
-def currenvySearch(search):
+def currencySearch(search):
 	# result = twder.now(userInput)
 	# text = ''
 	# # text += '時間：{}\n現金買入：{}\n現金賣出：{}\n即期買入：{}\n即期賣出：{}\n'.format(result[0], result[1], result[2], result[3],result[4])
