27cb3769ac60079849c5ee9822967617053b3d8f
Bill <1061241239@nkust.edu>
2019-08-02 11:15:54 +0800

    map

--- app.py
+++ app.py
@@ -75,8 +75,8 @@ def handle_message(event):
     weatherResult = OWMLonLatsearch(userLon,userLat)
     AQIResult = AQImonitor(userLon,userLat)
     gammaResult = gammamonitor(userLon,userLat)
-    message = TextSendMessage(text='地址：{}\n緯度：{}\n經度：{}\n'.format(userAdd,userLat,userLon))
-    message = TextSendMessage(text='天氣狀況：\n{}\n空氣品質：\n{}\n輻射值：\n{}\n'.format(weatherResult, AQIResult, gammaResult))
+    # message = TextSendMessage(text='地址：{}\n緯度：{}\n經度：{}\n'.format(userAdd,userLat,userLon))
+    message = TextSendMessage(text='☼天氣狀況：\n{}\n☁空氣品質：\n{}\n☀輻射值：\n{}\n'.format(weatherResult, AQIResult, gammaResult))
     line_bot_api.reply_message(event.reply_token, message)
 
 import os
