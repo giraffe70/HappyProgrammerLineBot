de2a5b3b14d49956acff5c94a104209f06faa7ce
Bill <1061241239@nkust.edu>
2019-08-03 17:52:11 +0800

    ibus

--- app.py
+++ app.py
@@ -104,8 +104,8 @@ def handle_message(event):
     elif userSend == '公車-{}'.format(userSend[3:]):
         routeList = showRouteList(userSend[3:])
         message = TextSendMessage(text=routeList)
-    elif userSend == '公車-{},{}'.format(userSend.split('-')[1].split(','), userSend,split(',')[1]):
-        routeResult = showRouteList(userSend.split('-')[1].split(','), int(userSend,split(',')[1]))
+    elif userSend == '公車-{},{}'.format(userSend.split('-')[1].split(',')[0], userSend,split(',')[1]):
+        routeResult = showRouteList(userSend.split('-')[1].split(',')[0], int(userSend,split(',')[1]))
         message = TextSendMessage(text=routeResult)
 
 
