846e589a6c13d93d3343fde6ca353bb0c9b2fd84
Bill <1061241239@nkust.edu>
2019-08-03 18:15:14 +0800

    ibus

--- app.py
+++ app.py
@@ -104,11 +104,11 @@ def handle_message(event):
     elif userSend == '公車-{}'.format(userSend.split('-')[1]):
         routeList = showRouteList(userSend.split('-')[1])
         message = TextSendMessage(text=routeList)
-    elif userSend == '公車-{},{}'.format(userSend.split('-')[1].split(',')[0], userSend.split(',')[1]):
-        routeResult = showRouteResult(userSend.split('-')[1].split(',')[0], int(userSend.split(',')[1]))
+    elif userSend == '公車-{}&{}'.format(userSend.split('-')[1].split('&')[0], userSend.split('&')[1]):
+        routeResult = showRouteResult(userSend.split('-')[1].split('&')[0], int(userSend.split('&')[1]))
         # route1 = userSend.split('-')[1].split(',')[0]
         # route2 = userSend.split(',')[1]
-        message = TextSendMessage(text=route1+route2)
+        message = TextSendMessage(text=routeResult)
 
 
     elif userSend == '匯率':
