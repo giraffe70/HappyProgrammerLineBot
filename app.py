fa4662fbe34f06e967639e0bb6fe2f3f50c3d772
Bill <1061241239@nkust.edu>
2019-08-03 21:18:29 +0800

    ibus

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ from linebot.models import *
 
 from engine.currencySearch import *
 from engine.OpenWeatherMap import OWMLonLatsearch
-from engine.AQI import AQImonitor
+from engine.aqi import AQImonitor
 from engine.gamma import gammamonitor
 from engine.SpotifyScrap import scrapSpotify
 from engine.crawlerArtical import *
@@ -104,8 +104,8 @@ def handle_message(event):
     elif userSend == '公車-{}'.format(userSend.split('-')[1]):
         routeList = showRouteList(userSend.split('-')[1])
         message = TextSendMessage(text=routeList)
-    elif userSend == '公車時刻-{}&{}'.format(userSend.split('-')[1].split('&')[0], userSend.split('&')[1]):
-        routeResult = showRouteResult(userSend.split('-')[1].split('&')[0], int(userSend.split('&')[1]))
+    elif userSend == '公車時刻-{},{}'.format(userSend.split('-')[1].split(',')[0], userSend.split(',')[1]):
+        routeResult = showRouteResult(userSend.split('-')[1].split(',')[0], int(userSend.split(',')[1]))
         message = TextSendMessage(text=routeResult)
 
 
