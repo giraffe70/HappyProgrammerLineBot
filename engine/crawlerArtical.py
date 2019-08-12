19f2659e24e6013ab5ceb2009e9da6c0f8329384
Bill <1061241239@nkust.edu>
2019-08-12 14:22:32 +0800

    spotify

--- engine/crawlerArtical.py
+++ engine/crawlerArtical.py
@@ -67,32 +67,7 @@ def Spotify_TOP30(url):
 		# 下載圖片
 		# download(bigImgLink(imgLink_big), clearName(songName))
 		
-		if index == 29:
-			break
-	return result
-
-# spotify(Taiwan)
-def Spotify_TAIWAN(url):
-	# url = 'https://spotifycharts.com/regional/tw/daily/latest'
-	webContent = requests.get(url)
-	webContent.encoding ='utf-8'
-	soup = BeautifulSoup(webContent.text, 'html.parser')
-	result = ''
-	for index,t in enumerate(soup.select('tbody tr')):
-		player = t.select('td')[3].text.split('by ')[1]
-		songName = t.select('td')[3].strong.text
-		imgLink = t.select('td img')[0]['src']
-		imgLink_big = t.select('.chart-table-image a')[0]['href']
-
-		result += '排名：{}\n'.format(t.select('td')[1].text)
-		result += '歌名：{}\n'.format(songName)
-		result += '歌手：{}\n圖片連結：{}\n'.format(player, imgLink)
-		result += '歌曲連結：{}\n'.format(imgLink_big)
-		result += '\n----------------------------------------------------------------------\n'
-		# 下載圖片
-		# download(bigImgLink(imgLink_big), clearName(songName))
-		
-		if index == 29:
+		if index == 49:
 			break
 	return result
 
