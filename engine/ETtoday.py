5ee4b7f011b236afec7944a1367689218d7befc1
Bill <1061241239@nkust.edu>
2019-08-19 10:07:22 +0800

    News

--- engine/ETtoday.py
+++ engine/ETtoday.py
@@ -2,7 +2,7 @@ import requests
 from bs4 import BeautifulSoup
 
 def crawerETtoday(url):
-	res = requests.get(url)
+	res = requests.post(url)
 	soup = BeautifulSoup(res.text, "html.parser")
 	# result = '焦點新聞：\n\n'
 	result = ''
@@ -19,8 +19,8 @@ def crawerETtoday(url):
 	# 				result += '{}\n{}\n\n'.format(realtime.find('a').text, realtime.find('a')['href'])
 	return result
 
-url = 'https://www.ettoday.net/'
-print(crawerETtoday(url))
+# url = 'https://www.ettoday.net/'
+# print(crawerETtoday(url))
 
 
 # print('焦點新聞：\n')
