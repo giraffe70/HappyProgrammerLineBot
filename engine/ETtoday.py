fbf92b4da2121052e9de9df7af1ddafa2f1ccc02
Bill <1061241239@nkust.edu>
2019-08-19 10:22:19 +0800

    News

--- engine/ETtoday.py
+++ engine/ETtoday.py
@@ -2,7 +2,9 @@ import requests
 from bs4 import BeautifulSoup
 
 def crawerETtoday(url):
-	res = requests.post(url)
+	
+	headers = {'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/76.0.3809.100 Safari/537.36' }
+	res = requests.get(url, headers=headers)
 	soup = BeautifulSoup(res.text, "html.parser")
 	# result = '焦點新聞：\n\n'
 	result = ''
@@ -11,6 +13,7 @@ def crawerETtoday(url):
 			if focus.find('a') != None:
 				result += '{}\n'.format(focus.find('a').text)
 				result += 'https://www.ettoday.net{}\n\n'.format(focus.find('a')['href'])
+
 	# # result += '------------------------------\n\n及時人氣：\n\n'
 	# for s in soup.select('.realtime-hot'):
 	# 	for realtime in s.select('.box_2 h3'):
@@ -19,8 +22,8 @@ def crawerETtoday(url):
 	# 				result += '{}\n{}\n\n'.format(realtime.find('a').text, realtime.find('a')['href'])
 	return result
 
-# url = 'https://www.ettoday.net/'
-# print(crawerETtoday(url))
+url = 'https://www.ettoday.net/'
+print(crawerETtoday(url))
 
 
 # print('焦點新聞：\n')
