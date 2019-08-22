554d462518b8d6f91a2d474ea9c108c51d39ef9e
Bill <1061241239@nkust.edu>
2019-08-22 13:35:32 +0800

    shopee

--- engine/shopee.py
+++ engine/shopee.py
@@ -18,7 +18,8 @@ def crawler_shopee(keyword):
 		name = s.find_all("div", class_="_1NoI8_ _2gr36I")[0].text
 		price = s.find_all("div", class_="_1w9jLI _37ge-4 _2XtIUk")[0].text
 		link = s.find_all("a")[0]['href']
-		result += '{}\n{}\n\n'.format(name, price)
+		if len(result) < 1900:
+			result += '{}\n{}\n\n'.format(name, price)
 	return result
  
 # sortby = 'relevancy' # 綜合排名
